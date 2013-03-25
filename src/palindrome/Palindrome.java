b8b936f28de6c534b94b20ca4cbaa9a5df15ca4a
Zachary Pratt Gibbs <Zachary Pratt Gibbs@MasterControl>
2013-03-25 17:48:24 -0600

    Final ready to turn in.

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -1,11 +1,22 @@
 /**
  * Name: Zachary Pratt Gibbs
- * Assignment: 
+ * Assignment: Palindrome
+ * File: Palindrome.java
+ * Date: March 25, 2013
+ * 
+ * Description: This program tests a string to see if it is a palindrome.
+ * It turns the string into a array of characters and puts it into
+ * a Stack and a Queue and then takes them off one by one comparing them to each
+ * other. As the first on is the last off with the stack and the first off with
+ * the queue if they are equal then the string is a palindrome.
  */
 package palindrome;
 
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Palindrome
 {
@@ -20,20 +31,26 @@ public class Palindrome
         int length = word.length();
         char[] wordChar = word.toCharArray();
         Stack<Character> stk = new Stack<>();
+        LinkedTransferQueue<Character> que = new LinkedTransferQueue<>();
         
         for(int index = 0; index < length; index++)
         {
             stk.push(wordChar[index]);
+            que.add(wordChar[index]);
         }
         for(int index = 0; index < length; index++)
         {
-            if(stk.pop() == wordChar[index])
-            {
-                //nada
-            }
-            else
-            {
-                return false;
+            try {
+                if(stk.pop() == que.take())
+                {
+                    //nada
+                }
+                else
+                {
+                    return false;
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Palindrome.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
         return true;
