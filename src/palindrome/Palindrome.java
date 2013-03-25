e30172ceb47b458db9f2ac2db1dea300aa071584
Zachary Pratt Gibbs <Zachary Pratt Gibbs@MasterControl>
2013-03-25 11:56:55 -0600

    Need to implement a queue instead of an array then finish my comments.

--- src/palindrome/Palindrome.java
+++ src/palindrome/Palindrome.java
@@ -1,40 +1,67 @@
+/**
+ * Name: Zachary Pratt Gibbs
+ * Assignment: 
+ */
 package palindrome;
+
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Palindrome
 {
-    public static boolean isPalindrome(String frase)
-	{
-        Iterator itr = new Iterator(word);
-		Stack stk = new Stack();
-		Queue que = new Queue();
-		while(itr.hasNext())
-		{
-			char letter = itr.nextChar();
-			if(letter == " ")
-			{
-				
-			}
-			else
-			{
-				stk.push(letter);
-				que.insert(letter);
-			}
-		}
-		for(int index = 0; index < word.length; index++)
-		{
-			if(stk.pop() == que.remove())
-			{
-				
-			}
-			else
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	public static void main(String[] args)
-	{
-		
-	}
+    /**
+     * Test to see if a string is a palindrome.
+     * @param word The string to test if it is a palindrome
+     * @return true if word is a palindrome or false if it isn't
+     */
+    public static boolean isPalindrome(String word)
+    {
+        word = word.toLowerCase().replaceAll("\\W", "");
+        int length = word.length();
+        char[] wordChar = word.toCharArray();
+        Stack<Character> stk = new Stack<>();
+        
+        for(int index = 0; index < length; index++)
+        {
+            stk.push(wordChar[index]);
+        }
+        for(int index = 0; index < length; index++)
+        {
+            if(stk.pop() == wordChar[index])
+            {
+                //nada
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * Main method to get input from a user to test for a palindrome.
+     */
+    public static void main(String[] args)
+    {
+        Scanner scr = new Scanner(System.in);
+        String another = "y";
+        String word = "";
+        while(another.equalsIgnoreCase("y"))
+        {
+            System.out.println("Please input a string to test if it is a palindrome");
+            word = scr.nextLine();
+            System.out.print("\"" + word + "\" is ");
+            if(!isPalindrome(word))
+            {
+                System.out.print("not ");
+            }
+            System.out.println("a palindrome.");
+            System.out.println("Would you like to play again? (y/n) [y]");
+            another = scr.nextLine();
+            if(another.equals(""))
+            {
+                another = "y";
+            }
+        }
+    }
 }
